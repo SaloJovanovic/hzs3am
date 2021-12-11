a16574878991dc0bdf1a04a36c8d9f2f26b17c25
acabutulija <aca.butulija@gmail.com>
2021-12-11 18:14:33 +0100

    Poruka za poruku. ruka za ruka. Srce za srce.

--- backend/src/main/java/am/recharge/backend/controllers/UserController.java
+++ backend/src/main/java/am/recharge/backend/controllers/UserController.java
@@ -1,11 +1,9 @@
 package am.recharge.backend.controllers;
 
+import am.recharge.backend.modules.User;
 import am.recharge.backend.services.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,5 +14,10 @@ public class UserController {
     @PostMapping("/verification")
     public String sendVerificationEmail(@RequestParam String email) {
         return userService.sendVerificationEmail(email);
+    }
+
+    @PostMapping("/create-new")
+    public User createNewUser (@RequestBody User userBody, @RequestParam int code) {
+        return userService.createNewUser(userBody, code);
     }
 }
